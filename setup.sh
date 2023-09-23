793bf14f67112991eca3095269c375f0f104652a
Gautam Mishra <gautam.mishra@hotmail.com>
2023-09-23 04:30:45 -0500

    configure pyenv, install python and set finder defaults

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ print_green() {
     printf "${GREEN}$1${NC}\n"
 }
 
+sudo -v
+
 print_green "Installing Command Line Tools"
 xcode-select --install
 
@@ -17,6 +19,7 @@ fi
 
 # Install dev tools
 print_green "Installing Developer Tools"
+brew tap spring-io/tap
 apps=(
     iterm2
     pyenv
@@ -27,12 +30,14 @@ apps=(
     jq
     git
     tree
+    spring-boot
 )
 brew install ${apps[@]}
 
 print_green "Installing Developer Apps"
 casks=(
     1password
+    1password-cli
     google-chrome
     visual-studio-code
     github
@@ -42,6 +47,7 @@ casks=(
     postman
     intellij-idea-ce
     pycharm-ce
+    vnc-viewer
 )
 brew install --cask ${casks[@]}
 
@@ -53,13 +59,10 @@ fi
 # Install PowerLevel10k
 if [[ ! -f "$(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" ]] && [[ ! -d "~/.oh-my-zsh/custom/themes/powerlevel10k" ]]; then
     brew install powerlevel10k
-    cat >>~/.zshrc <<EOL
-
-source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme
-
-# To customize prompt, run \`p10k configure\` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-EOL
+    echo '\n# Powerlevel10k configuration' >> ~/.zshrc
+    echo 'source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+    echo '\n# To customize prompt, run \`p10k configure\` or edit ~/.p10k.zsh.' >> ~/.zshrc
+    echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc
 fi
 
 if test -f ~/.p10k.zsh; then
@@ -70,6 +73,19 @@ cp .p10k.zsh ~/
 
 # brew tap homebrew/cask-fonts
 # brew install font-hack-nerd-font
+
+# Configure PyEnv and install Python
+if ! grep -wq "pyenv init -" ~/.zshrc; then
+    echo '\n# PyEnv Configuration' >> ~/.zshrc
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+    echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+    echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+fi
+PYTHON_VERSION="3.11.5"
+print_green "Installing python $PYTHON_VERSION and setting as default"
+pyenv install -s $PYTHON_VERSION
+pyenv global $PYTHON_VERSION
 
 # Install applications
 print_green "Installing Other Apps"
@@ -90,3 +106,8 @@ code --install-extension ms-python.python
 
 # Remove Chrome Apps
 rm -rf ~/Applications/Chrome\ Apps.localized/*
+
+# Setting defaults
+print_green "Setting Defaults"
+defaults write com.apple.finder ShowStatusBar -bool true            # Finder: Show status bar
+defaults write com.apple.finder ShowPathbar -bool true              # Finder: Show path bar
